872eb7c496dc27ef474f08944178fc19f0547af9
RyotaMitaraiWeb <titanium5624@gmail.com>
2024-12-24 02:40:47 +0200

    update linter again

--- eslint.config.js
+++ eslint.config.js
@@ -3,22 +3,32 @@ import js from '@eslint/js';
 import stylistic from '@stylistic/eslint-plugin';
 
 export default [
-  ...eslintPluginAstro.configs['all'],
-  js.configs.recommended,
-  {
-    rules: {
-      'astro/no-set-html-directive': 'error',
-      'astro/semi': 'error',
-      '@stylistic/quotes': ['error', 'single', { allowTemplateLiterals: true }],
-      '@stylistic/semi': 'error',
-      'sx-one-expression-per-line': 'off',
-      '@stylistic/member-delimiter-style': 'error',
-      '@stylistic/eol-last': 'error',
-      '@stylistic/max-len': ['error', { code: 80, ignoreStrings: true }],
-      '@stylistic/quote-props': ['error', 'consistent-as-needed'],
-    },
-    plugins: {
-      '@stylistic': stylistic,
-    },
-  },
+    ...eslintPluginAstro.configs['all'],
+    stylistic.configs['all-flat'],
+    js.configs.recommended,
+    {
+        rules: {
+            'astro/no-set-html-directive': 'error',
+            'astro/semi': 'error',
+            '@stylistic/quotes': [
+                'error',
+                'single',
+                {allowTemplateLiterals: true}
+            ],
+            'jsx-one-expression-per-line': 'off',
+            '@stylistic/eol-last': 'error',
+            '@stylistic/max-len': [
+                'error',
+                {code: 80,
+                    ignoreStrings: true}
+            ],
+            '@stylistic/quote-props': [
+                'error',
+                'consistent-as-needed'
+            ]
+        },
+        plugins: {
+            '@stylistic': stylistic
+        }
+    }
 ];
